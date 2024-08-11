5f81f13c1e9eed9d3fe91a4e20b61b95b4fef41a
lou57810 <bernard.oswald@free.fr>
2024-08-11 23:03:06 +0200

    Annul trigger_error.

--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.http import HttpResponse
 from . import views
-from sentry_sdk import capture_message, capture_exception, set_tag
+from sentry_sdk import capture_exception, set_tag
 # from django.conf.urls import handler404, handler500
 
 
@@ -19,18 +19,8 @@ def large_resource(request):
     return HttpResponse("Done!")
 
 
-def trigger_error(request):
-    try:
-        division_by_zero = 1 / 0
-    except Exception as e:
-        set_tag("Opération non permise!")
-        capture_exception(e)
-
-
-
 urlpatterns = [
     path('', index, name='home'),
-    path('sentry-debug/', trigger_error),
     path('error/', views.custom500, name='oc_lettings_error'),
     path('error', views.custom500, name='oc_lettings_error'),
     path("lettings/", include("lettings.urls")),
